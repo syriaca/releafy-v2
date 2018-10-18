fbc4a437e50fa471f5e0929d5fee0a34e393fb5e
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-10-18 12:38:14 +0200

    Added a script for heroku

--- server.js
+++ server.js
@@ -23,6 +23,17 @@ mongoose
 
 app.use('/api/users', users);
 
-const port = process.env.PORT || 5000;
+//Behaviour for production env.
+if (process.env.NODE_ENV === "production") {
+    // Express will serve up production assets
+    //like our main.js file, or main.css file
+    app.use(express.static("client/build"));
+    //Express will serve index.html file if it doesn't recognize the route
+    const path = require("path");
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
