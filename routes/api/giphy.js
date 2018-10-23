519b1d1c3f52fb66f861accbed44f3eb1ce34732
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-10-23 11:26:27 +0200

    Indentation

--- routes/api/giphy.js
+++ routes/api/giphy.js
@@ -7,12 +7,10 @@ const router = express.Router();
 // @Desc    Get giphy 
 // @Access  Private
 router.get('/giphy/:query', (req, res) => {
-    var options = {
-        json: true
-        };
-        request.get(`https://api.giphy.com/v1/gifs/search?q=${req.params.query}&limit=3&api_key=${keys.giphyKey}`, options, function(error, response, body) {
-            res.json(response.body.data);
-        });
+    var options = {json: true};
+    request.get(`https://api.giphy.com/v1/gifs/search?q=${req.params.query}&limit=3&api_key=${keys.giphyKey}`, options, function(error, response, body) {
+        res.json(response.body.data);
+    });
 });
 
 module.exports = router;
